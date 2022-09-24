d661c3ad069ba5ae01dc848036b1cdaeeeb459d8
Mateus Ferreira Canedo <mateusferreiracanedo@gmail.com>
2022-09-24 20:06:24 -0300

    fix

--- prisma/migrations/20220923140420_nitt/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "contact" DROP CONSTRAINT "contact_owner_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "contact" ADD CONSTRAINT "contact_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
