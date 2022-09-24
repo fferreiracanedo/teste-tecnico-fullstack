5b796df958bda33212a4424e0363089d94893e14
Mateus Ferreira Canedo <mateusferreiracanedo@gmail.com>
2022-09-24 19:21:31 -0300

    fix deploy

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
 import "express-async-errors"
 import express from 'express'
 import cors from 'cors'
+import swaggerUi from "swagger-ui-express"
 import handleError from './middleware/appError.middewares'
 import userRoutes from './routes/user.routes'
 import contactsRouter from './routes/contacts.routes'
 const app = express()
 
 app.use(express.json())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup())
 app.use(cors())
 app.use('/users', userRoutes)
 app.use('/contacts', contactsRouter)
 
-
-
 app.use(handleError)
-
 
 
 export default app
